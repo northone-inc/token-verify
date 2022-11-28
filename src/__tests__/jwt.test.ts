4f092b8c5fd245808664ed2d5608335265ff6011
Blake E <blake@northone.com>
2022-11-28 16:39:19 -0500

    rename CI workflow and resolve lint test

--- src/__tests__/jwt.test.ts
+++ src/__tests__/jwt.test.ts
@@ -1,5 +1,5 @@
 import createJWKSMock from 'mock-jwks'
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import * as SourceModule from '../jwt'
 
 const testAudience = 'private'
